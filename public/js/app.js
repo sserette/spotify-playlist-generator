55c8ce5d8dfe05f6cfe4b4027fde72421c6a561b
Steven Serette <sserette@gmail.com>
2018-03-28 23:04:54 -0400

    added download to csv button to tables, needs to be fixed for file extensions

--- public/js/app.js
+++ public/js/app.js
@@ -97,5 +97,20 @@ $(document).ready(function() {
 			console.log(data);
 			populateTable("search-table", data.tracks);
 		});
+	});
+	
+	$('table').each(function () {
+        var $table = $(this);
+
+        var $button = $("<button type='button'>");
+        $button.text("Export to spreadsheet");
+        $button.insertBefore($table);
+
+        $button.click(function () {
+            var csv = $table.table2CSV({
+                delivery: 'value'
+            });
+            window.location.href = 'data:text/csv;charset=UTF-8,' + encodeURIComponent(csv);
+        });
     });
 });
